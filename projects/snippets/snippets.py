51bc6198ca7645c4e7e8021d0b7209f3e82571e6
Derrick Lin <drrckln@gmail.com>
2014-11-01 23:47:58 -0400

    add retrieval abilities

--- projects/snippets/snippets.py
+++ projects/snippets/snippets.py
@@ -16,6 +16,10 @@ def make_parser():
     put_parser.add_argument("snippet", help="The snippet text")
     put_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
 
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+    get_parser.add_argument("filename", default="snippets.csv", nargs="?", help="The snippet filename")
+
     return parser
 
 # Set the log output file, and the log level
@@ -31,19 +35,39 @@ def put(name, snippet, filename):
     logging.debug("Write successful")
     return name, snippet
 
+def get(name, filename):
+    logging.info("Reading {!r} from {!r}".format(name, filename))
+    logging.debug("Reading file")
+    result = "This snippet name is nonexistent."
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        logging.debug("Reading snippet from file")
+        flag = False
+        for a,b in reader:
+            if a == name:
+                result = b
+                logging.debug("Read successful")
+                flag = True
+                break
+        if not flag:
+            logging.debug("Read unsuccessful")
+    return name, result
+
 def main():
     logging.info("starting snippets")
     parser = make_parser()
     arguments = parser.parse_args(sys.argv[1:])
 
-    print arguments
     arguments = vars(arguments)
-    print arguments
     command = arguments.pop("command")
 
     if command == "put":
         name, snippet = put(**arguments)
         print "Stored {!r} as {!r}".format(snippet, name)
+
+    if command == "get":
+        name, snippet = get(**arguments)
+        print "Retrieved {!r}: {!r}".format(name, snippet)
 
 if __name__ == "__main__":
     main()
